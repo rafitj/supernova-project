288d24f076b835dc95cf8266319916b3342b4f24
rafitj <rafit.jamil@gmail.com>
2020-04-18 15:03:01 -0400

    add more car methods

--- park.py
+++ park.py
@@ -9,7 +9,23 @@ class Car(threading.Thread):
         self.name = f'Car #{num}'
         self.parked = True
         self.capacity = 5
+        self.atStation = True
+        self.filled = False
+        self.numPeople = 0
+        self.station = station
 
+    def ride(self):
+        print(f'{self.name} starting ride')
+        time.sleep(2)
+        print(f'{self.name} finished ride')
+        self.filled = False
+        self.parked = True
+        self.numPeople = 0
+
+    def loadPerson(self):
+        self.numPeople += 1
+        if self.numPeople == self.capacity:
+            self.filled = True
 
 
 class Station(threading.Thread):
@@ -21,11 +37,11 @@ class Station(threading.Thread):
         self.numCars = numCars
         self.numPpl = 0
 
-    # def simulate(self):
-    #     self.start()
-    #     for i in range(22):
-    #         self.spawnPerson(i)
-    #         time.sleep(randint(0, 3))
+    def simulate(self):
+        self.start()
+        for i in range(22):
+            self.spawnPerson(i)
+            time.sleep(randint(0, 3))
 
     def spawnPerson(self, i):
         print(f'Spawned person {i}')
